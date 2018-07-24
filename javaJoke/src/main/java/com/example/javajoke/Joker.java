d7b081ac375d97e80e6ddb0dda37937316cad56f
Simone Armadoro <draturan@gmail.com>
2018-07-24 23:12:00 +0200

    last correction for the review

--- javaJoke/src/main/java/com/example/javajoke/Joker.java
+++ javaJoke/src/main/java/com/example/javajoke/Joker.java
@@ -1,8 +1,18 @@
 package com.example.javajoke;
 
+import java.util.Random;
+
 public class Joker {
 
+    private final String[] jokes = {
+            "This is a funny joke",
+            "This is also a funny joke",
+            "And this one too!",
+            "Here is the joke! No joke!"
+    };
+
     public String gimmeAJoke(){
-        return "Here is the joke! No joke!";
+        int index = new Random().nextInt(jokes.length);
+        return jokes[index];
     }
 }
